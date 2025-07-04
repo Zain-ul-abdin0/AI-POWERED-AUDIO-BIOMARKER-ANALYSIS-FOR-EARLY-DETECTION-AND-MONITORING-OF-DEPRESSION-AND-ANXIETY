ae76d9f7cc14a81946038b2936150f4b494e6481
Zain-ul-abdin0 <ahmza147@gmail.com>
2025-07-04 15:00:18 +0200

    extra code is removed

--- LoadModel.py
+++ LoadModel.py
@@ -255,14 +255,14 @@ def load_model_pipeline():
     try:
         # Change this path to load the specific model you want to test:
         # e.g., 'svm_model.joblib', 'random_forest_model.joblib', 'lightgbm_model.joblib', or 'ensemble_model.joblib'
-        model_to_load = 'lightgbm_model.joblib' 
+        model_to_load = 'random_forest_model.joblib' 
         pipeline = joblib.load(os.path.join('./trained_models', model_to_load))
         print(f"✔ Full model pipeline '{model_to_load}' loaded successfully")
         
         # Load the corresponding feature names.
         # If loading 'ensemble_model.joblib', it's best to use 'overall_selected_features.joblib'
         # as the ensemble itself doesn't have a single set of feature names.
-        if model_to_load == 'lightgbm_model.joblib':
+        if model_to_load == 'svm_model.joblib':
             feature_names_file = './trained_models/lightgbm_features.joblib'
         else:
             feature_names_file = os.path.join('./trained_models', model_to_load.replace('_model.joblib', '_features.joblib'))
